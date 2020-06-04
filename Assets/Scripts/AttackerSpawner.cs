39224641d077c5c88482b5abddbaa4033c6c1ef4
Pitsillides Charalambos <harryspitsillides@hotmail.com>
2020-06-04 19:33:12 +0100

    Add defender

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour {
+
+    [SerializeField]
+    private float minSpawnDelay = 1f;
+
+    [SerializeField]
+    private float maxSpawnDelay = 5f;
+
+    [SerializeField]
+    private Attacker attacker;
+    
+    private bool _spawn = true;    
+    
+    // Start is called before the first frame update
+    IEnumerator Start() {
+        while (_spawn) {
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+            Spawn();
+        }
+    }
+
+    private void Spawn() {
+        Instantiate(attacker, transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update() { }
+
+}
