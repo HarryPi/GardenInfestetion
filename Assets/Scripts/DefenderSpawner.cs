c841008c1c42fe291cc65bd250b0d1978196ebe0
Pitsillides Charalambos <harryspitsillides@hotmail.com>
2020-06-04 22:21:00 +0100

    Allow to spawn defenders

---
+++ Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject defender;
+
+    private void OnMouseDown() {
+        SpawnDefender();
+    }
+
+    private void SpawnDefender() {
+        Vector2 mouseLoc = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(mouseLoc);
+        GameObject gameDefender = Instantiate(defender, worldPos, Quaternion.identity);
+    }
+}
